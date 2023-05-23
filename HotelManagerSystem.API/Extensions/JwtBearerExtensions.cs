5f6ded5dab5fc63412499330c9f752aebf960cf5
kaimov <mike@magic.org>
2023-05-23 23:08:37 +0600

    fix using

--- HotelManagerSystem.API/Extensions/JwtBearerExtensions.cs
+++ HotelManagerSystem.API/Extensions/JwtBearerExtensions.cs
@@ -3,7 +3,6 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Text;
-using DorgramApi.WebAPI.AuthBL.Models;
 using HotelManagerSystem.API.AuthBL.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
